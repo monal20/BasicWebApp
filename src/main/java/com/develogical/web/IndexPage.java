e1c522cf35d5c9d2e3a2f23e427c7f9584cf0c60
Mona Lin <monalin2002@gmail.com>
2022-09-21 19:46:22 -0400

    Added text to Index Page

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,7 +7,8 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
-                "<h1>Welcome!</h1>" +
+                "<h1>Welcome! </h1>" + 
+                "<p> Added a change <p>" +
                 "<p>Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
